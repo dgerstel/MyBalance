99b2ed6ad2e872376846ed1c3d573942e11f91e7
Tomasz <gerstel.tomasz@gmail.com>
2021-05-17 16:06:07 +0200

    small changes in css, app.prop, home.test

--- src/test/java/com/mybalance/HomeControllerTest.java
+++ src/test/java/com/mybalance/HomeControllerTest.java
@@ -11,35 +11,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.mybalance.controller.HomeController;
-import com.mybalance.service.UserService;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-//@WebMvcTest(HomeController.class)
+
 class HomeControllerTest {
 
 	@Autowired
 	private MockMvc mockMvc;
-	
-	@Autowired
-	UserService userService;
-	
-//	  @MockBean
 
-	
 	@Test
-	public void testHomePage() throws Exception{
-		mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(view().name("index")).andExpect(content().string(containsString("MyBalance")));
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(view().name("index"))
+				.andExpect(content().string(containsString("MyBalance")));
 	}
-
-
 }
